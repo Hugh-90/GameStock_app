1fb80eb03427b0f8acfce19b0c86e46151121181
hugh <hugh90@hotmail.es>
2021-09-15 21:15:11 +0200

    fixed one bug

---
+++ src/data.js
@@ -0,0 +1,83 @@
+const data = {
+    products: [
+        {
+            _id:"0",
+            name:"Spiderman Insomniac",
+            category:"game",
+            image:"/images/p1.jpg",
+            price:60,
+            brand:"PS4",
+            rating:5,
+            numReviews:10,
+            description: "great videogame"
+        },
+        {
+            _id:"1",
+            name:"Batman Telltale",
+            category:"game",
+            image:"/images/p2.jpg",
+            price:30,
+            brand:"PS4",
+            rating:4.5,
+            numReviews:10,
+            description: "great story telling videogame"
+        },
+        {
+            _id:"2",
+            name:"Devil May Cry V",
+            category:"game",
+            image:"/images/p4.jpg",
+            price:60,
+            brand:"PS4",
+            rating:5,
+            numReviews:10,
+            description: "great videogame"
+        },
+        {
+            _id:"3",
+            name:"Final Fantasy VII Remake",
+            category:"game",
+            image:"/images/p7.jpg",
+            price:60,
+            brand:"PS5",
+            rating:5,
+            numReviews:200,
+            description: "great remake"
+        },
+        {
+            _id:"4",
+            name:"Spiderman Miles Morales",
+            category:"game",
+            image:"/images/p6.jpg",
+            price:70,
+            brand:"PS5",
+            rating:5,
+            numReviews:20,
+            description: "great sequel"
+        },
+        {
+            _id:"5",
+            name:"Batman Arkham Collection",
+            category:"game",
+            image:"/images/p3.jpg",
+            price:60,
+            brand:"PS4",
+            rating:5,
+            numReviews:10,
+            description: "great saga"
+        },
+        {
+            _id:"6",
+            name:"Batman Return to Arkham",
+            category:"game",
+            image:"/images/p5.jpg",
+            price:40,
+            brand:"PS4",
+            rating:5,
+            numReviews:10,
+            description: "great remaster"
+        }
+    ]
+};
+
+export default data;
