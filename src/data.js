13b0ff1f344ede9689795993130a58decb937f4d
hugh <hugh90@hotmail.es>
2021-09-16 17:50:50 +0200

    implemented screens and routes, updated the data file and added styles

--- src/data.js
+++ src/data.js
@@ -6,10 +6,11 @@ const data = {
             category:"game",
             image:"/images/p1.jpg",
             price:60,
+            countInStock:100,
             brand:"PS4",
             rating:5,
             numReviews:10,
-            description: "great videogame"
+            description: "A Brand-New and Authentic Spider-Man Adventure.This is an experienced Peter Parker who is more masterful at fighting big crime in New York City. At the same time, he's struggling to balance his chaotic personal life and career while the fate of millions of New Yorkers rest upon his shoulders."
         },
         {
             _id:"1",
@@ -17,10 +18,11 @@ const data = {
             category:"game",
             image:"/images/p2.jpg",
             price:30,
+            countInStock:10,
             brand:"PS4",
             rating:4.5,
             numReviews:10,
-            description: "great story telling videogame"
+            description: "Batman: The Telltale Series, like most Batman centered media, is set within Gotham, a city infested with both crime and corruption. The time period which the series is set seems to be the mid-to-late 2010s, with technologies such as drones and smartphones being used by characters."
         },
         {
             _id:"2",
@@ -28,10 +30,11 @@ const data = {
             category:"game",
             image:"/images/p4.jpg",
             price:60,
+            countInStock:50,
             brand:"PS4",
             rating:5,
             numReviews:10,
-            description: "great videogame"
+            description: "The game takes place five years after Devil May Cry 4 and follows a trio of warriors with demonic powers: the returning Dante, Nero and a new protagonist named V as they attempt to stop the Demon King Urizen from destroying the human world."
         },
         {
             _id:"3",
@@ -39,10 +42,11 @@ const data = {
             category:"game",
             image:"/images/p7.jpg",
             price:60,
+            countInStock:0,
             brand:"PS5",
             rating:5,
             numReviews:200,
-            description: "great remake"
+            description: "Final Fantasy VII Remake is a 2020 action role-playing game developed and published by Square Enix. It is the first in a planned series of games remaking the 1997 PlayStation game Final Fantasy VII. Set in the dystopian cyberpunk metropolis of Midgar, players control mercenary Cloud Strife."
         },
         {
             _id:"4",
@@ -50,10 +54,11 @@ const data = {
             category:"game",
             image:"/images/p6.jpg",
             price:70,
+            countInStock:15,
             brand:"PS5",
             rating:5,
             numReviews:20,
-            description: "great sequel"
+            description: "In the latest adventure in the Marvel’s Spider-Man universe, teenager Miles Morales is adjusting to his new home while following in the footsteps of his mentor, Peter Parker, as a new Spider-Man. But when a fierce power struggle threatens to destroy his new home, the aspiring hero realizes that with great power, there must also come great responsibility. To save all of Marvel’s New York, Miles must take up the mantle of Spider-Man and own it. Includes a voucher code for Marvel’s Spider-Man: Remastered."
         },
         {
             _id:"5",
@@ -61,10 +66,12 @@ const data = {
             category:"game",
             image:"/images/p3.jpg",
             price:60,
+            countInStock:90,
+            countInStock:60,
             brand:"PS4",
             rating:5,
             numReviews:10,
-            description: "great saga"
+            description: "The Batman Arkham collection brings you the definitive versions of Rocksteady's Arkham trilogy games, including all post-launch content, in one complete collection. Experience two of the most critically acclaimed titles of the last generation - Batman: Arkham Asylum and Batman: Arkham City, with fully remastered and updated visuals. Complete your experience with the Explosive finale to the Arkham series In Batman: Arkham Knight. Become the Batman and utilise a wide range of gadgets and abilities to face off against Gotham's most dangerous villains, finally facing the ultimate threat against the city that Batman is sworn to protect."
         },
         {
             _id:"6",
@@ -72,10 +79,11 @@ const data = {
             category:"game",
             image:"/images/p5.jpg",
             price:40,
+            countInStock:80,
             brand:"PS4",
             rating:5,
             numReviews:10,
-            description: "great remaster"
+            description: "Return to Arkham and experience two of the most critically acclaimed titles of the last generation - Batman: Arkham Asylum and Batman: Arkham City, with fully remastered and updated visuals. Batman: Return to Arkham includes the comprehensive versions of both games and includes all previously released additional content. Batman: Arkham Asylum exposes players to a dark and atmospheric adventure that takes them into the depths of Arkham Asylum -Gotham City's high security psychiatric hospital for the criminally insane. Use Batman's legendary gadgetry, strike from the shadows, and confront The Joker and Gotham City's most notorious super villains. Batman: Arkham City sends players soaring into Arkham City - a heavily fortified sprawling district in the heart of Gotham City. Batman Arkham City introduces a brand-new story that draws together an all-star cast of classic characters and murderous villains from the Batman universe."
         }
     ]
 };
