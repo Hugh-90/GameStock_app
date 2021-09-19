37b196182d5c7086d19bfbc32604c6aa14784b5c
hugh <hugh90@hotmail.es>
2021-09-19 19:43:35 +0200

    created two users in mongodb database

--- BackEnd/data.js
+++ BackEnd/data.js
@@ -1,4 +1,20 @@
+import bcrypt from "bcryptjs";
 const data = {
+    users: [
+        {
+            name: "Hugh",
+            email:"admin@example.com",
+            password:bcrypt.hashSync("1234",8),
+            isAdmin: true,
+        },
+        {
+            name: "James",
+            email:"user@example.com",
+            password:bcrypt.hashSync("1234",8),
+            isAdmin: false,
+        },
+    ],
+
     products: [
         {
             _id:"0",
