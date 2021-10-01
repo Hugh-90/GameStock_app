6918a2308ddab77dcfe4b07b4d8737387a965759
hugh <hugh90@hotmail.es>
2021-10-01 18:02:42 +0200

    In userEditScreen I created a new screen to create a form for updating user info, in userListScreen dispatch user details and reset to make it possible to go back and foward between user list and user edit screen, and also in user actions,reducer and store implemented userEdit

--- FrontEnd/src/screens/UserListScreen.js
+++ FrontEnd/src/screens/UserListScreen.js
@@ -1,10 +1,12 @@
+import { PromiseProvider } from "mongoose";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteUser, listUsers } from "../actions/userActions";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
+import { USER_DETAILS_RESET } from "../constants/userConstants";
 
-export default function UserListScreen() {
+export default function UserListScreen(props) {
     const userList = useSelector(state => state.userList);
     const {loading, error, users} = userList;
 
@@ -14,6 +16,7 @@ export default function UserListScreen() {
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(listUsers());
+        dispatch({type: USER_DETAILS_RESET});
     }, [dispatch, successDelete]);
 
     const deleteHandler = (user) => {
@@ -53,7 +56,7 @@ return(
                                 <td>{user.email}</td>
                                 <td>{user.isAdmin ? "YES" : "NO"}</td>
                                 <td>
-                                    <button type="button" className="small">Edit</button>
+                                    <button type="button" className="small" onClick={() => props.history.push(`/user/${user._id}/edit`)}>Edit</button>
                                     <button type="button" className="small" onClick={()=> deleteHandler(user)}>Delete</button>
                                 </td>
                             </tr>
